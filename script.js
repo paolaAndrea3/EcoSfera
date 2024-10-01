b8616b52e70745941cba573b3ad2b8961f3a1b4e
Sebaxtian18 <zsebax.1994@gmail.com>
2024-10-01 09:32:23 -0500

    Se agrgan las subcategorias de bienestar utilizando objetos

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 // Datos de las categorías y subcategorías
+class subcategoria {
+    constructor({nombre, productos = []}) {
+        this.nombre = nombre;
+        this.productos = productos;
+    }
+}
+const aseoPersonal = new subcategoria({
+    nombre: 'Aseo personal',
+    productos: [{producto: 'Jabones corporales naturales', precio: 9000},
+        {producto: 'Cepillo dental banbu', precio: 7500}, 
+        {producto: 'Desodorante natural en barra', precio: 1500}]
+})
+const maquillaje = new subcategoria({
+    nombre: 'Maquillaje',
+    productos: [{producto: 'Corrector de ojeras organico en barra', precio: 59000},
+        {producto: 'Perfume solido en polvo', precio: 31000}, 
+        {producto: 'Rubor compacto organico', precio: 54900}]
+})
+const cuidadoDeLaPiel = new subcategoria({
+    nombre: 'Cuidado de la piel',
+    productos: [{producto: 'Tratamiento para cejas y pestañas', precio: 22300},
+        {producto: 'Crema humectante', precio: 46000}, 
+        {producto: 'Balsamo para bebé', precio: 32400}]
+})
 const catalogo = [
     {
         nombre: 'Bienestar',
-        subcategorias: ['Salud mental', 'Fitness', 'Nutrición']
+        subcategorias: [aseoPersonal, maquillaje, cuidadoDeLaPiel],
     },
     {
         nombre: 'Aseo',
@@ -32,8 +56,30 @@ function mostrarSubcategorias(indice) {
         case 1:
             console.log(`Subcategorías de ${catalogo[0].nombre}:`);
             for (let i = 0; i < catalogo[0].subcategorias.length; i++) {
-                console.log(` - ${catalogo[0].subcategorias[i]}`);
+                console.log(` - ${catalogo[0].subcategorias[i].nombre}`);
             }
+            let repetir = true
+            do {
+            let opcion = Number(prompt('Ingresar a \n1- Aseo personal \n2- Maquillaje \n3- Cuidado de la piel'))
+                switch (opcion) {
+                    case 1:
+                        console.log(aseoPersonal.productos);
+                        break;
+                    case 1:
+                        console.log(maquillaje.productos);
+                        break;
+                    case 1:
+                        console.log(cuidadoDeLaPiel.productos);
+                        break;
+                    default:
+                        console.log('Categria no definida')
+                        break;
+                }
+                repetir = Number(prompt('Desea salir del menu \n 1- Si \n 2- No'))
+                if (repetir == 1) {
+                    repetir = false
+                }
+            } while (repetir);    
             break;
         case 2:
             console.log(`Subcategorías de ${catalogo[1].nombre}:`);
@@ -67,4 +113,5 @@ const seleccion = parseInt(prompt("Elige una categoría \n 1. Bienestar \n 2. As
 
 // Mostrar subcategorías de la categoría seleccionada
 mostrarSubcategorias(seleccion);
+
 
