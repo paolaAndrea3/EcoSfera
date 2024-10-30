7a059ebf21885e4ea3e74062b07c707e29356ab5
Sebaxtian18 <zsebax.1994@gmail.com>
2024-10-30 06:28:47 -0500

    Creacion de una base de datos llamada (productos) con objetos y valores quemados para utilizarla de base en los algoritmos de busqueda y agregar ala carrito

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const productos = [{
+    nombrePoducto: 'Jabones de mano naturales',
+    precio: 5900,
+    descripcion: 'lorem ipsum',
+    categoria: 'Aseo personal'
+},
+{
+    nombrePoducto: 'Cepillo de dienntes ecologico',
+    precio: 13900,
+    descripcion: 'lorem ipsum',
+    categoria, 'Aseo personal'
+},
+{
+    nombrePoducto: 'Rubor pasta dura',
+    precio: 301900,
+    descripcion: 'lorem ipsum',
+    categoria: 'Maquillaje'
+},
+{
+    nombrePoducto: 'shampoo antipulgas para perro',
+    precio: 19900,
+    descripcion: 'lorem ipsum',
+    categoria: 'Mascotas'
+}]
+
 function busquedaDeProductos() {
     /*Carol rivera*/
     // Crear una funcion que permita al usuario hacer una busqueda de productos. 
