9e9d096d72ccd5f17122f16153e647ba23b8bd49
paolaAndrea3 <murillojimenezpaola1@gmail.com>
2024-11-13 14:25:31 -0500

    Se agregaron productos en el index y la funcion de añadir carrito en el script

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 import { usuarios } from "./database.js";
 
+//---------------funcion de anadir al carrito
+
+function añadirAlCarrito(idMensaje) {
+    // Obtener el elemento del mensaje de confirmación usando el ID específico para cada producto.
+    const mensajeConfirmacion = document.getElementById(idMensaje);
+    
+    // Mostrar el mensaje de confirmación, quitando la clase "hidden" que lo oculta.
+    mensajeConfirmacion.classList.remove("hidden");
+    
+    // Ocultar el mensaje después de 2 segundos.
+    esperaUnMomento(() => {
+        mensajeConfirmacion.classList.add("hidden");
+    }, 2000); // Espera de 2 segundos (2000 milisegundos)
+}
+
+// Función de espera personalizada que utiliza "setTimeout" para ejecutar una acción después de un tiempo.
+function esperaUnMomento(callback, tiempo) {  //callback: una función a ejecutar después de cierto tiempo.
+    setTimeout(callback, tiempo);
+}
+//--------------------------------------------------
 //Constantes botones
 const btnLogin = document.getElementById("btnLogin");
 const btnRegistro = document.getElementById("btnRegistro");
